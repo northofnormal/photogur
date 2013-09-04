ad179697cee21bbee7004ee1ea0d5d7cffb07ca0
Anne <northofnormal@gmail.com>
2013-09-03 21:14:22 -0400

    Making pictures show at /pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,20 @@
 class PicturesController < ApplicationController
 	def index
+		@pictures = [
+			{
+				:title => "The old church on the coast of the White Sea",
+				:artist => "Sergey Ershov", 
+				:url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+				}, 
+				{
+					:title => "Sea Power", 
+					:artist => "Stephen Scullion", 
+					:url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
+					},
+					{
+						:title => "Into the Poppies",
+						:artist => "John Wilhem",
+						:url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+						}]
 	end
 end
